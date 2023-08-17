c48adbf5e2910d1018811d6bd5b92180d8cc24d2
Mugta1 <142477055+Mugta1@users.noreply.github.com>
2023-08-17 22:48:30 +0530

    Learnt how to read videos

--- learningCV2.py
+++ learningCV2.py
@@ -5,5 +5,11 @@ import cv2 as cv
 cv.imshow('gow', img)'''
 
 ## reading videos
-vid=cv.VideoCapture("1")
-cv.waitKey(0)
+vid=cv.VideoCapture("D:/python/videos/God of War 2023.07.23 - 21.12.01.02.DVR.mp4")
+while True:
+    isTrue, frame = vid.read()
+    cv.imshow('vid', frame)
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+vid.release
+cv.destroyAllWindows()
--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+##some error was coming while opening files. used this to test it.and
 import cv2 as cv
 img=cv.imread('photos/gow.png')
 cv.imshow('gow', img)

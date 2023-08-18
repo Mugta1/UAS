663bde65cbe1682176d4044c1e4f659e9c2b2974
Mugta1 <142477055+Mugta1@users.noreply.github.com>
2023-08-18 23:38:01 +0530

    color channels

---
+++ colorchannels.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+img=cv.imread('photos/1.png')
+b,g,r= cv.split(img)
+cv.imshow('og', img)
+cv.imshow('blue', b)
+cv.imshow('green', g)
+cv.imshow("red", r)
+print(img.shape)
+print(b.shape)
+print(r.shape)
+print(g.shape)
+blank=np.zeros(img.shape[:2], dtype='uint8')
+merged= cv.merge([b,blank,blank])
+cv.imshow("merged", merged)
+cv.waitKey(0)

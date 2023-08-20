65c22df372af87bf894d7ee7f3b4afaf7a36494a
Mugta1 <142477055+Mugta1@users.noreply.github.com>
2023-08-21 00:43:03 +0530

    final.

--- finalscript/final.py
+++ finalscript/final.py
@@ -3,31 +3,29 @@ import numpy as np
 two=[]
 three=[]
 four=[]
-append=[]
-input=[]
-imglist=['photos/1.png']
-dict={}
+dictxyz={}
+#function for color change as asked in 1st question
+def colorchange(i):
+    img = cv.imread(i)
+    brown_lower = np.array([10, 50, 50])
+    brown_upper = np.array([30, 255, 255])
+    green_lower = np.array([35, 50, 50])
+    green_upper = np.array([85, 255, 255])
+    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    brown_mask = cv.inRange(hsv_img, brown_lower, brown_upper)
+    green_mask = cv.inRange(hsv_img, green_lower, green_upper)
+    yellow_color = np.array([60, 255, 255])
+    img[np.where(brown_mask)] = yellow_color
+    blue_color = np.array([120, 255, 255])
+    img[np.where(green_mask)] = blue_color
+    result_img = cv.cvtColor(img, cv.COLOR_HSV2BGR)
+    blur=cv.bilateralFilter(result_img, 100,100,100)
+    cv.imwrite('edited_img.jpg', blur)
+    cv.imshow('Edited img', result_img)
+    cv.waitKey(0)
 
 
-def colorchange(imglist):
-    for i in imglist:
-        img = cv.imread(i)
-        brown_lower = np.array([10, 50, 50])
-        brown_upper = np.array([30, 255, 255])
-        green_lower = np.array([35, 50, 50])
-        green_upper = np.array([85, 255, 255])
-        hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-        brown_mask = cv.inRange(hsv_img, brown_lower, brown_upper)
-        green_mask = cv.inRange(hsv_img, green_lower, green_upper)
-        yellow_color = np.array([60, 255, 255])
-        img[np.where(brown_mask)] = yellow_color
-        blue_color = np.array([120, 255, 255])
-        img[np.where(green_mask)] = blue_color
-        result_img = cv.cvtColor(img, cv.COLOR_HSV2BGR)
-        blur=cv.bilateralFilter(result_img, 100,100,100)
-        cv.imwrite('edited_img.jpg', blur)
-        cv.imshow('Edited img', result_img)
-        cv.waitKey(0)
+#function for doing all the triangle visualization questions (2,3,4)
 def values(i):
     img=cv.imread(i)
     bi=cv.bilateralFilter(img, 100, 100 ,100)
@@ -92,15 +90,30 @@ def values(i):
     two.append([HF, HG])
     three.append([PF, PG])
     four.append(PR)
-    
+    dictxyz[i]=PR
 
 
+#defining a final function that calculates everything
+
+def final(i):
+    colorchange(i)
+    values(i)
+
+
+
+final('photos/1.png')
+final('photos/2.png')
+final('photos/3.png')
+final('photos/4.png')
+final('photos/5.png')
 
 
 print(two)
 print(three)
 print(four)
-    
+print(dictxyz)
 
+sorted = sorted(dictxyz, key=dictxyz.get, reverse=True)
 
+print(sorted)
 

4de49211e8f4e97913c3aeb241b36784bdb60e6a
Mugta1 <142477055+Mugta1@users.noreply.github.com>
2023-08-20 23:31:08 +0530

    final working

--- finalscript/after.py
+++ finalscript/after.py
@@ -5,12 +5,13 @@ three=[]
 four=[]
 append=[]
 input=[]
-imglist=['photos/1.png', 'photos/2.png', 'photos/3.png','photos/4.png', 'photos/5.png', 'photos/6.png', 'photos/7.png', 'photos/8.png', 'photos/10.png', 'photos//11.png']
+imglist=['photos/1.png']
 dict={}
+
+
 def colorchange(imglist):
     for i in imglist:
         img = cv.imread(i)
-        cv.imshow('img', img)
         brown_lower = np.array([10, 50, 50])
         brown_upper = np.array([30, 255, 255])
         green_lower = np.array([35, 50, 50])
@@ -27,18 +28,72 @@ def colorchange(imglist):
         cv.imwrite('edited_img.jpg', blur)
         cv.imshow('Edited img', result_img)
         cv.waitKey(0)
+def values(i):
+    img=cv.imread(i)
+    bi=cv.bilateralFilter(img, 100, 100 ,100)
+    b,g,r= cv.split(bi)
+    retr, threshb=cv.threshold(b, 128, 255, cv.THRESH_BINARY)
+    retr, threshr=cv.threshold(r, 128, 255, cv.THRESH_BINARY)
+
+    #create masks for burnt and unburnt area
+
+    hsv=cv.cvtColor(bi, cv.COLOR_BGR2HSV)
+    LG=np.array([35,50,50])
+    UG=np.array([85,255,255])
+
+    #values of hsv used above were taken from findinghsv.py under meainscript folder in the repo
+    #making masks
+    greenmask=cv.inRange(hsv, LG, UG)
+
+    kernel = np.ones((5,5), np.uint8)
+
+    greenmask=cv.morphologyEx(greenmask, cv.MORPH_CLOSE, kernel)
+
+    greenmask=cv.bitwise_not(greenmask)
+
+
+    cannygrass=cv.Canny(greenmask, 125, 175 )
+    cannyr=cv.Canny(threshr, 125, 175)
+    cannyb=cv.Canny(threshb, 125, 175)
+
+    commonrg=cv.bitwise_and(cannygrass, cannyr)
+    commonbg=cv.bitwise_and(cannyb, cannygrass)
+    ##calculating the number of blue and red houses
+    contourR, heirarchies= cv.findContours(cannyr,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE )
+    TR=int((len(contourR))/2)
+    contourB, heirarchies= cv.findContours(cannyb,cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE )
+    TB=int((len(contourB))/2)
+
+    #number of red houses not on fire
+    contour1, heirarchies = cv.findContours(commonrg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    RG=int((len(contour1))/3)
+
+    #number of blue houses not on fire
+    contour2, heirarchies= cv.findContours(commonbg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    BG=int((len(contour2)/3))
+
+    #number of red houses on fire
+    RF=TR - RG
+
+    #number of blue houses on fire
+    BF=TB - BG
+
+    ##printing the number of houses
+    valuelist=[RF, BF, RG, BG]
+    return valuelist
+
 
 def output(imglist):
     for i in imglist:
-        #try1.py will return values
+        final=values(i)
         #number of red houses on fire
-        rf=4
+        rf=final[0]
         #number of blue houses on fire
-        bf=2
+        bf=final[1]
         #number of red houses not on fire
-        rg=1
+        rg=final[2]
         #number of blue houses not on fire
-        bg=3
+        bg=final[3]
         name=i ##a passed as name of file in parameter of the function
         Hb=rf+bf
         Hg=rg+bg
@@ -69,6 +124,11 @@ sorted_keys = sorted(dict.keys(), key=lambda key: dict[key], reverse=True)
 for key in sorted_keys:
     print(key)
 
+def allcode(imglist):
+    colorchange(imglist)
+    collectoutput(imglist)
+
+    
 
 
     
